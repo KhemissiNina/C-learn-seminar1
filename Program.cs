93891e197bff47d175cf170812c17a4d35781b56
Nina <samninvik@mail.ru>
2022-10-11 00:16:37 +0500

    решили вторую задачу

--- Program.cs
+++ Program.cs
@@ -1,4 +1,16 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.Write("Введите число:");
+/*Console.Write("Введите число:");
 int number = int.Parse(Console.ReadLine());
 Console.WriteLine($"квадрат {number} равен {number*number}");
+*/
+int num1 = int.Parse(Console.ReadLine());
+int num2 = int.Parse(Console.ReadLine());
+if (num2*num2==num1)
+    Console.WriteLine($"{num1} квадрат {num2}");
+else
+    Console.WriteLine("Нет, не является");
+int koren = Convert.ToInt32(Math.Sqrt(num1));
+if (koren == num2)
+    Console.WriteLine($"{num1} квадрат {num2}");
+else
+    Console.WriteLine("Нет, не является");
